4f8cdad3bd8fbdc811b874fa17440dcdaa397873
akumasugi <102664326+akumasugi@users.noreply.github.com>
2022-04-07 19:30:26 +0530

    Update casino.ts

--- src/commands/Coding/casino.ts
+++ src/commands/Coding/casino.ts
@@ -7,9 +7,9 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'casino',
-            aliases: ['support,casino'],
-            description: 'Gets the gambling group links',
+            command: 'akuma',
+            aliases: ['support,akuma'],
+            description: 'Gets the group links',
             category: 'extras',
             usage: `${client.config.prefix}casino`,
             baseXp: 10
@@ -19,13 +19,12 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         (await this.client.sendMessage(
         M.sender.jid,
-                `*🍁Casino Group🍁*\n\n
-                 *Lord Savage🕊️: Fun Group*:https://chat.whatsapp.com/EOB2UescDz78rTYG4MQWe1\n\n
-                 *Lord Savage🕊️ Gambling: 𝗖𝗮𝘀𝗶𝗻𝗼 *:https://chat.whatsapp.com/Gy1JemBGs9wLjVEfzbRJ21                      `,
+                `*🔥akuma Group🔥*\n\n
+                 *akuma🔥: Fun Group*:https://chat.whatsapp.com/H6aIaXmMLglGL7VnekVYDG\n\n
            MessageType.text
         ))
         const n = [
-            'https://telegra.ph/file/e44db02b0b240224a6bcc.mp4'
+            'https://c.tenor.com/0OCyLe8flawAAAPo/natsu.mp4'
         ]
         let rin = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: rin }, MessageType.video, {quoted:M.WAMessage,
