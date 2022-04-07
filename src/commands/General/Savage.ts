edfcb09c8d38bb88d1e6f87f752029a9a5631122
akumasugi <102664326+akumasugi@users.noreply.github.com>
2022-04-07 20:27:54 +0530

    Update Savage.ts

--- src/commands/General/Savage.ts
+++ src/commands/General/Savage.ts
@@ -8,8 +8,8 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'savage',
-            description: 'Displays info about savage.',
+            command: 'akuma',
+            description: 'Displays info about akuma.',
             category: 'general',
             usage: `${client.config.prefix}savage`
         })
@@ -17,23 +17,19 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         const n = [
-            'https://telegra.ph/file/8eed1568a71b3b55dac05.jpg','https://telegra.ph/file/6777fe4846a3b0af4decd.jpg'
+            'https://i.ibb.co/fCg5nzQ/IMG-20220407-WA0369.jpg'
         ]
         let rin = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: rin }, MessageType.image, {quoted:M.WAMessage,
             mimetype: Mimetype.jpeg,
-            caption: `🍭𝗵𝗲𝗹𝗹𝗼!🍃I'm Lord Savage 🕊️ a learner/student & an upcoming developer in the future.
+            caption: `🔥𝗵𝗲𝗹𝗹𝗼!🍃I'm akuma an ordinary person who loves watching anime.
             
-🍁𝙒𝙝𝙖𝙩𝙨𝘼𝙥𝙥;
-Wa.me/+263716928420
-Wa.me/+263716564623
+🔥𝙒𝙝𝙖𝙩𝙨𝘼𝙥𝙥;
+Wa.me/+917892202052
 
-📮𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢;
-https://instagram.com/_smokey_vines_
-https://instagram.com/amapiano_state
+🔥𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆;
+https://facebook.com/groups/600441174428472/
 
-🚀𝘿𝙞𝙨𝙘𝙤𝙧𝙙;
-｟𝖢𝗈𝗆𝗂𝗇𝗀 𝖲𝗈𝗈𝗇｠
 
 ⪼𝖲𝖾𝖾 𝗒𝖺𝗁 💘` }
         )
