257eed776764ea7bffc2fefbf3d11b836902969b
akumasugi <102664326+akumasugi@users.noreply.github.com>
2022-04-08 17:32:13 +0530

    Update Info.ts

--- src/commands/General/Info.ts
+++ src/commands/General/Info.ts
@@ -41,7 +41,7 @@ export default class Command extends BaseCommand {
 		}
 		this.run = async (M: typings.ISimplifiedMessage): Promise<void> => {
 			const chitoge =
-				"https://telegra.ph/file/913ab3710227ee3917bf1.mp4";
+				"https://telegra.ph/file/9e1e035be514052e12eeb.mp4";
 			return void this.client.sendMessage(
 				M.from,
 				{ url: chitoge },
@@ -49,9 +49,9 @@ export default class Command extends BaseCommand {
 				{
 					quoted: M.WAMessage,
 					mimetype: Mimetype.gif,
-					caption: `━━━❰ 🍭 *𝙉𝘼𝙏𝙎𝙐* 🍭 ❱━━━\n\n🔮 *Groups: ${
+					caption: `━━━❰ 🐲 *𝙉𝘼𝙏𝙎𝙐* 🐲 ❱━━━\n\n🔮 *Groups: ${
 				chats.length
-					}\n\n🚦 *Uptime: ${uptime()}*\n\n🦆 *My cute people: ${users}*\n\n\n💢 *Banned Users: ${uban}*\n\n\n`,
+					}\n\n🚦 *Uptime: ${uptime()}*\n\n🐉 *My cute people: ${users}*\n\n\n💢 *Banned Users: ${uban}*\n\n\n`,
 				}
 			);
 		};
