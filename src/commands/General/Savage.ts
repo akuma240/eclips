af8917a981612720bc25ba7925bcbbfdf3badef2
akumasugi <102664326+akumasugi@users.noreply.github.com>
2022-04-08 17:44:05 +0530

    Update Savage.ts

--- src/commands/General/Savage.ts
+++ src/commands/General/Savage.ts
@@ -17,20 +17,20 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         const n = [
-            'https://i.ibb.co/b51vz7M/Natsu.jpg'
+            'https://i.ibb.co/zXtHSYD/a1ce72a00be1fe93d701f265e49b8dc9.jpg'
         ]
         let rin = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: rin }, MessageType.image, {quoted:M.WAMessage,
             mimetype: Mimetype.jpeg,
-            caption: `🔥𝙃𝙀𝙇𝙇𝙊!❄️I'm 𝘼𝙆𝙐𝙈𝘼 an ordinary guy who loves watching anime & play games 🎮. I'm a BCA Student 🤍 relationship with 𝙈𝙄𝙍𝘼 𝙨𝙖𝙣 ❤️  
+            caption: `🔥𝙃𝙀𝙇𝙇𝙊!❄️I'm 𝘼𝙆𝙐𝙈𝘼 an ordinary guy who loves watching anime & play games🎮. I'm a BCA Student🤍relationship with 𝙈𝙄𝙍𝘼❤️  
             
-🔥𝙒𝙝𝙖𝙩𝙨𝘼𝙥𝙥;
+🍀𝙒𝙝𝙖𝙩𝙨𝘼𝙥𝙥;
 Wa.me/+917892202052
 
-🔥𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆;
+🍀𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆;
 https://facebook.com/groups/600441174428472/
  
-🔥𝙄𝙉𝙎𝙏𝘼𝙂𝙍𝘼𝙈;
+🍀𝙄𝙉𝙎𝙏𝘼𝙂𝙍𝘼𝙈;
 https://www.instagram.com/akuma__24/?hl=en
 
 ⪼𝖲𝖾𝖾 𝗒𝖺𝗁 💟` }
