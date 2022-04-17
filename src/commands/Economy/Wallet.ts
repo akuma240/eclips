1a446a5b967960f7d4e6ad87c63d9e7c27d8f7f0
akumasugi <102664326+akumasugi@users.noreply.github.com>
2022-04-17 17:04:21 +0530

    Update Wallet.ts

--- src/commands/Economy/Wallet.ts
+++ src/commands/Economy/Wallet.ts
@@ -28,7 +28,7 @@ export default class Command extends BaseCommand {
 
     const buttonMessage: any = {
       contentText: `👛 *Wallet | ${M.sender.username}*\n\n🪙 *Gold: ${result}*`,
-      footerText: "© 𝙀𝙡𝙖𝙞𝙣𝙖 2022",
+      footerText: "© 𝐍𝐀𝐓𝐒𝐔 2022",
       buttons: buttons,
       headerType: 1,
     };
