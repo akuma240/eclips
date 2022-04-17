6bd6fb8e8fbacd301f9c834bff385fc39087963e
akumasugi <102664326+akumasugi@users.noreply.github.com>
2022-04-17 18:15:03 +0530

    Update T2Pc.ts

--- src/commands/Pokemon/T2Pc.ts
+++ src/commands/Pokemon/T2Pc.ts
@@ -53,7 +53,7 @@ export default class Command extends BaseCommand {
     }
     const buttonMessage: any = {
       contentText: `You might want to check your party.`,
-      footerText: "© Elaina 2022",
+      footerText: "© 𝐍𝐀𝐓𝐒𝐔 2022",
       buttons: buttons,
       headerType: 1,
     };
