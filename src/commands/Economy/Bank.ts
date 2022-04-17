d65142591d1df8094020daa8c8ba62528ab4e3fb
akumasugi <102664326+akumasugi@users.noreply.github.com>
2022-04-17 17:03:03 +0530

    Update Bank.ts

--- src/commands/Economy/Bank.ts
+++ src/commands/Economy/Bank.ts
@@ -28,7 +28,7 @@ export default class Command extends BaseCommand {
 
     const buttonMessage: any = {
       contentText: `🏦 *Bank | ${M.sender.username}*\n\n🪙 *Gold: ${result}*`,
-      footerText: "© Natsu 2022",
+      footerText: "© 𝐍𝐀𝐓𝐒𝐔 2022",
       buttons: buttons,
       headerType: 1,
     };
