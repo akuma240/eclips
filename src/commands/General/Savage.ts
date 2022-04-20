cad0a66129abfee40aae6e8078ab0c71e9ec22d6
akumasugi <102664326+akumasugi@users.noreply.github.com>
2022-04-20 16:13:48 +0530

    Update Savage.ts

--- src/commands/General/Savage.ts
+++ src/commands/General/Savage.ts
@@ -17,7 +17,7 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         const n = [
-            'https://telegra.ph/file/3b819b2cb2be451775dde.mp4'
+            'https://telegra.ph/file/3b819b2cb2be451775dde.mp4','https://telegra.ph/file/293c68ca4a004fdd22c7f.mp4'
         ]
         let rin = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: rin }, MessageType.image, {quoted:M.WAMessage,
