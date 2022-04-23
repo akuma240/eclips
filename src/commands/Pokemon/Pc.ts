8d02c60ee87955c17ca17f1d1b4b216d261cc6c1
akumasugi <102664326+akumasugi@users.noreply.github.com>
2022-04-23 13:20:53 +0530

    Update Pc.ts

--- src/commands/Pokemon/Pc.ts
+++ src/commands/Pokemon/Pc.ts
@@ -46,7 +46,7 @@ export default class Command extends BaseCommand {
     }
     const buttonMessage: any = {
       contentText: `${text}`,
-      footerText: "© Elaina 2022",
+      footerText: "© 𝐍𝐀𝐓𝐒𝐔 2022",
       buttons: buttons,
       headerType: 1,
     };
