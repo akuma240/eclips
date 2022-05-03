76d24283fedfd50281dc685ab0b5aba49e206ffb
ETERNALGLITCH <68656546+NemasisDarkX@users.noreply.github.com>
2022-05-03 22:44:47 +0530

    Update Sticker.ts

--- src/commands/Utils/Sticker.ts
+++ src/commands/Utils/Sticker.ts
@@ -86,8 +86,8 @@ export default class Command extends BaseCommand {
       })();
       return {
         categories,
-        pack: pack[1] || "𝐙𝐄𝐑𝐎❤️",
-        author: pack[2] || "𝐀𝐊𝐔𝐌𝐀",
+        pack: pack[1] || "ETERNAL",
+        author: pack[2] || "GLITCH",
         quality,
         type: StickerTypes[
           parsedArgs.flags.includes("--crop") ||
